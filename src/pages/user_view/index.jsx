36e4f69a4f1286c23553cbb4348f2f716c628e67
madina875 <evaevasmq@gmail.com>
2025-09-07 17:02:56 +0500

    hmw

--- src/pages/user_view/index.jsx
+++ src/pages/user_view/index.jsx
@@ -1,7 +1,37 @@
-import React, { memo } from "react";
+import axios from "axios";
+import React, { memo, useEffect, useState } from "react";
 
 const UserView = () => {
-  return <div>UsersView</div>;
+  // const navigate = useNavigate();
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => setData(res.data));
+  }, []);
+  return (
+    <>
+      <div className="grid grid-cols-4 rounded-2xl mb-[4%] gap-[30px] p-[30px] bg-gray-100">
+        {data?.map((el) => (
+          <div key={el.id} className="rounded-2xl bg-white pt-[3%]">
+            <div
+              // onClick={() => navigate(`/user/${el.id}`)}
+              className="place-items-center cursor-cell"
+            ></div>
+            <div className="p-[4%] mt-[4%] place-items-center">
+              <p className="text-3xl">{el.name}</p>
+              <h3 className="text-2xl italic mt-[5%]">{el.phone}</h3>
+              <p className="text-[1.3rem] my-[2%]">email: {el.email}</p>
+              <p className="text-[1.3rem] my-[2%]">
+                address: {el.address.street}
+              </p>
+              <p className="text-[1.3rem] my-[2%]">website: {el.website}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default memo(UserView);
