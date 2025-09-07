5bffad185fdc19340860bf59675f669096a6774a
madina875 <evaevasmq@gmail.com>
2025-09-07 16:43:09 +0500

    hmw

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 
 const Posts = () => {
   return <div>Posts</div>;
--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { memo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SignIn = ({ data }) => {
